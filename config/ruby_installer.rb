89e6b6eb4d8bf01cd33b9d0f6d134a33046de21c
ashbb <ashbbb@gmail.com>
2010-01-23 14:23:07 +0900

    Edited a little to use Ruby 1.9.1p378 for building Shoes with MinGW.

--- config/ruby_installer.rb
+++ config/ruby_installer.rb
@@ -87,7 +87,7 @@ module RubyInstaller
     )
 
     Ruby19 = OpenStruct.new(
-      :release => "p0",
+      :release => "p378",
       :version => "1.9.1",
       :url => "http://ftp.ruby-lang.org/pub/ruby/1.9",
       :checkout => 'http://svn.ruby-lang.org/repos/ruby/branches/ruby_1_9_1',
@@ -102,7 +102,7 @@ module RubyInstaller
         "debugflags='-g3 -ggdb'"
       ],
       :files => [
-        'ruby-1.9.1-p0.tar.bz2'
+        'ruby-1.9.1-p378.tar.bz2'
       ],
       :dependencies => [
         'readline5.dll',
